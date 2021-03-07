7c70f24e536290d4503e2f56bbedf83c36d097fa
dothuhang <dothuhang156@gmail.com>
2021-03-07 22:59:01 +0100

    Add photo python

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+using namespace std;
 
 char version[]="1.0";
 
@@ -10,6 +11,23 @@ char const* greet() {
   return "hello, world";
 }
 
+class Company {
+  private:
+    float capital;
+    string address;
+  public:
+    Company() {}
+    Company(const string &address, const float &capital) : address(address), capital(capital) {}
+    const string &getAddress() const {
+      return address;
+    }
+    const float &getCapital() const {
+      return capital;
+    }
+    void setCapital(const string &capital_) { capital = capital_; }
+    void setAddress(const string &address_) { address = address_; }
+};
+
 namespace py = pybind11;
 
 
@@ -19,4 +37,11 @@ PYBIND11_MODULE(hello_component,greetings)
   
   greetings.def("greet", &greet, "a function saying hello");
   greetings.def("getVersion", &getVersion, "a function returning the version");
+  
+  py::class_<Company>(greetings, "Company",py::dynamic_attr())
+        .def(py::init<const string &, const float &>())
+        .def("setCapital", &Company::setCapital)
+        .def("getCapital", &Company::getCapital)
+        .def("setAddress", &Company::setAddress)
+        .def("getAddress", &Company::getAddress);
 }
