570a6e6a4799dbcdd3641a1a3c921d4987243b56
dothuhang <dothuhang156@gmail.com>
2021-05-29 20:35:21 +0000

    correction

--- cryptokey/cle.cpp
+++ cryptokey/cle.cpp
@@ -1,13 +1,10 @@
-#include "cle.h"
-#include "micro-eec/uECC.h"
+
+#include "micro-ecc/uECC.h"
 #include <pybind11/pybind11.h>
-using namespace std;
+
 
 char version[]="1.0";
 
-char const* getVersion() {
-	return version;
-}
 
 char const* getVersion() {
 	return version;
@@ -56,30 +53,57 @@ char *binToHexString(char *out,const unsigned char *bin, size_t len)
 }
 
 
+class Cle 
+{
+    private:
+	std::string publicKey;
+	std::string privateKey;
 
+    public:
 
-Cle::Cle() {
-	
-	initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0");
-	
-}
+	Cle() {}
 
-void Cle::initialize(std::string &nb) {
-	privateKey = nb;
+	~Cle() {}
+
+	void initialize(std::string &nb)
+        {
+	    privateKey = nb;
 	
-	uint8_t binaryPrivate[32];
-	hexStringToBin(binaryPrivate, privateKey.c_str());
+	    uint8_t binaryPrivate[32];
+	    hexStringToBin(binaryPrivate, privateKey.c_str());
 	
-	const int publicKeySize = uECC_curve_public_key_size(uECC_secp256k1());
+	    const int publicKeySize = uECC_curve_public_key_size(uECC_secp256k1());
 	
-	uint8_t *varIntPublicKey = new uint8_t[publicKeySize];
+	    uint8_t *varIntPublicKey = new uint8_t[publicKeySize];
 	
-	uECC_compute_public_key(binaryPrivate, varIntPublicKey, uECC_secp256k1());
+	    uECC_compute_public_key(binaryPrivate, varIntPublicKey, uECC_secp256k1());
 	
-	char hexPublicKey[128];
+	    char hexPublicKey[128];
 	
-	binToHexString(hexPublicKey, varIntPublicKey, 64);
+	    binToHexString(hexPublicKey, varIntPublicKey, 64);
 	
-	publicKey = std::string(hexPublicKey, 128);
+	    publicKey = std::string(hexPublicKey, 128);
 		
+	}
+
+	const std::string &getPrivateKey() { return privateKey; }
+	const std::string &getPublicKey() { return publicKey; }
+};
+
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(cle,greetings)
+{
+    greetings.doc() = "greeting_object 1.0";
+    greetings.def("getVersion", &getVersion, "a function returning the version");
+
+    py::class_<Cle>(greetings, "Cle", py::dynamic_attr())
+	    .def(py::init())
+	    .def("initialize", &Cle::initialize)
+	    .def("getPrivateKey", &Cle::getPrivateKey)
+	    .def("getPublicKey", &Cle::getPublicKey);
+
 }
+
+
